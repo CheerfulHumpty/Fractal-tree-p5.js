0d97edd4cf2a08e9827b76c81995fce08439e2d8
Sathwik Nayak <nayaksathwik2204@gmail.com>
2024-12-20 18:03:01 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,48 @@
+       var angle;
+       var ang;
+       var prevtime = 0
+        function setup(){
+            createCanvas(600,600)
+            function angval(timeframe){
+                var diff = (timeframe-prevtime)/1000
+                window.requestAnimationFrame(angval)
+                if(diff<0.2){
+                    return
+                }
+                else{
+                    ang = Math.random()*(0.79-0.76)+0.73
+                    prevtime = timeframe
+                }
+            }
+            window.requestAnimationFrame(angval)
+        }
+        function draw(){
+            background(51)
+            angle = ang
+            translate(300,height)
+            stroke(255)
+            branch(150,6)
+        }
+        function branch(len,weight){
+            strokeWeight(weight)
+           line(0,0,0,-len)
+           translate(0,-len)
+           if(len>8){
+           push()
+           rotate(angle)
+           branch(len*0.67,weight*0.47)
+           pop()
+           push()
+           rotate(-angle)
+           branch(len*0.67,weight*0.37)
+           pop()
+           push()
+           rotate(angle/2)
+           branch(len*0.67,weight*0.9)
+           pop()
+           push()
+           rotate(angle/6)
+           branch(len*0.67,weight*0.57)
+           pop()
+           }
+        }
